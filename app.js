efcdac02dfab306926e8267302a22bed4b69e27a
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-29 19:55:20 -0300

    primera prueba de multiplayer

---
+++ app.js
@@ -0,0 +1,295 @@
+const mongoDB = require("./connections/connection");
+require("dotenv/config");
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT;
+
+// mongoDB();
+// Middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
+const room1 = [];
+const room2 = [];
+const room3 = [];
+const room4 = [];
+
+let jugadasRoom1 = [];
+let jugadasRoom2 = [];
+let jugadasRoom3 = [];
+let jugadasRoom4 = [];
+
+io.on("connection", (socket) => {
+  // Creacion de usuario
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    for (let user of users) {
+      if (user.username == socket.username) {
+        let err = {
+          msg: "Usuario ya existe",
+        };
+        io.to(id).emit("error", err);
+        socket.disconnect();
+        return;
+      }
+    }
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  // Si todo sale bien conecta y avisa que se conecto
+  console.log(`User ${socket.username} connected`);
+  socket.emit("users", users);
+  let salas = {
+    rooms: [room1, room2, room3, room4],
+  };
+  // Le envia datos de las salas
+  io.to(socket.id).emit("datos de salas", salas);
+  // Conexion a salas
+  socket.on("conectarRoom1", (msg) => {
+    if (room1.length >= 2) {
+      let err = {
+        msg: "Room 1 lleno",
+      };
+      io.to(msg.id).emit("error", err);
+    } else {
+      room1.push(msg.username);
+      let Message = {
+        msg: "Conected to room 1",
+        users: room1,
+        room: 1,
+      };
+      socket.join("room1");
+      io.to("room1").emit("connected to room", Message);
+      let conn = {
+        msg: `Nuevo usuario conectado`,
+        room: 1,
+        users: room1,
+      };
+      io.emit("new connection room", conn);
+    }
+  });
+  socket.on("conectarRoom2", (msg) => {
+    if (room2.length >= 2) {
+      let err = {
+        msg: "Room 2 lleno",
+      };
+      io.to(msg.id).emit("error", err);
+    } else {
+      room2.push(msg.username);
+      let Message = {
+        msg: "Conected to room 2",
+        users: room2,
+        room: 2,
+      };
+      socket.join("room2");
+      io.to("room2").emit("connected to room", Message);
+      let conn = {
+        msg: `Nuevo usuario conectado`,
+        room: 2,
+        users: room2,
+      };
+      io.emit("new connection room", conn);
+    }
+  });
+  socket.on("conectarRoom3", (msg) => {
+    if (room3.length >= 2) {
+      let err = {
+        msg: "Room 3 lleno",
+      };
+      io.to(msg.id).emit("error", err);
+    } else {
+      room3.push(msg.username);
+      let Message = {
+        msg: "Conected to room 3",
+        users: room3,
+        room: 3,
+      };
+      socket.join("room3");
+      io.to("room3").emit("connected to room", Message);
+      let conn = {
+        msg: `Nuevo usuario conectado`,
+        room: 3,
+        users: room3,
+      };
+      io.emit("new connection room", conn);
+    }
+  });
+  socket.on("conectarRoom4", (msg) => {
+    if (room4.length >= 2) {
+      let err = {
+        msg: "Room 4 lleno",
+      };
+      io.to(msg.id).emit("error", err);
+    } else {
+      room4.push(msg.username);
+      let Message = "";
+      if (room4.length == 1) {
+        Message = {
+          msg: "Conected to room 4",
+          users: room4,
+          room: 4,
+          startPlaying: false,
+        };
+      } else {
+        Message = {
+          msg: "Conected to room 4",
+          users: room4,
+          room: 4,
+          startPlaying: true,
+        };
+      }
+      socket.join("room4");
+      io.to("room4").emit("connected to room", Message);
+      let conn = {
+        msg: `Nuevo usuario conectado`,
+        room: 4,
+        users: room4,
+      };
+      io.emit("new connection room", conn);
+    }
+  });
+  // En partida
+  socket.on("jugada", (msg) => {
+    switch (msg.room) {
+      case 1:
+        if (room1.length == 2) {
+          jugadasRoom1.push({ jugada: msg.jugada, username: msg.username });
+          console.log("Jugadas de room 1: ");
+          console.log(jugadasRoom1);
+          if (jugadasRoom1.length == 2) {
+            let resultado = resolverPartida(jugadasRoom1[0], jugadasRoom1[1]);
+            console.log(`Emitiendo resultado, ganador... ${resultado}`);
+            io.to(`room1`).emit("resultado", resultado);
+            jugadasRoom1 = [];
+          }
+        } else {
+          let msg = {
+            msg: `Faltan jugadores para iniciar`,
+          };
+          io.to(socket.id).emit("error", msg);
+        }
+        break;
+      case 2:
+        if (room2.length == 2) {
+          jugadasRoom2.push({ jugada: msg.jugada, username: msg.username });
+          if (jugadasRoom2.length == 2) {
+            let resultado = resolverPartida(jugadasRoom2[0], jugadasRoom2[1]);
+            io.to(`room2`).emit("resultado", resultado);
+            jugadasRoom2 = [];
+          }
+        } else {
+          let msg = {
+            msg: `Faltan jugadores para iniciar`,
+          };
+          io.to(socket.id).emit("error", msg);
+        }
+        break;
+      case 3:
+        if (room3.length == 2) {
+          jugadasRoom3.push({ jugada: msg.jugada, username: msg.username });
+          if (jugadasRoom3.length == 2) {
+            let resultado = resolverPartida(jugadasRoom3[0], jugadasRoom3[1]);
+            io.to(`room3`).emit("resultado", resultado);
+            jugadasRoom3 = [];
+          }
+        } else {
+          let msg = {
+            msg: `Faltan jugadores para iniciar`,
+          };
+          io.to(socket.id).emit("error", msg);
+        }
+        break;
+      case 4:
+        if (room4.length == 2) {
+          jugadasRoom4.push({ jugada: msg.jugada, username: msg.username });
+          if (jugadasRoom4.length == 2) {
+            let resultado = resolverPartida(jugadasRoom4[0], jugadasRoom4[1]);
+            io.to(`room4`).emit("resultado", resultado);
+            jugadasRoom4 = [];
+          }
+        } else {
+          let msg = {
+            msg: `Faltan jugadores para iniciar`,
+          };
+          io.to(socket.id).emit("error", msg);
+        }
+        break;
+    }
+  });
+
+  // Al desconectarse
+  socket.on("disconnect", () => {
+    console.log(`Usuario ${socket.username} se desconecto`);
+    let rooms = [room1, room2, room3, room4];
+    for (let room of rooms) {
+      if (room.includes(socket.username)) {
+        let index = room.indexOf(socket.username);
+        if (index == 0) {
+          room = room.splice(0, 1);
+        } else {
+          room = room.pop();
+        }
+      }
+    }
+    let salas = {
+      rooms: rooms,
+    };
+    socket.broadcast.emit("datos de salas", salas);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+function resolverPartida(JugadorUno, JugadorDos) {
+  let ganador = "";
+  switch (JugadorUno.jugada) {
+    case "Piedra":
+      if (JugadorDos.jugada == "Papel") {
+        ganador = JugadorDos.username;
+      } else if (JugadorDos.jugada == "Tijera") {
+        ganador = JugadorUno.username;
+      } else if (JugadorDos.jugada == "Piedra") {
+        ganador = "Empate";
+      }
+      break;
+
+    case "Papel":
+      if (JugadorDos.jugada == "Papel") {
+        ganador = "Empate";
+      } else if (JugadorDos.jugada == "Tijera") {
+        ganador = JugadorDos.username;
+      } else if (JugadorDos.jugada == "Piedra") {
+        ganador = JugadorUno.username;
+      }
+      break;
+
+    case "Tijera":
+      if (JugadorDos.jugada == "Papel") {
+        ganador = JugadorUno.username;
+      } else if (JugadorDos.jugada == "Tijera") {
+        ganador = "Empate";
+      } else if (JugadorDos.jugada == "Piedra") {
+        ganador = JugadorDos.username;
+      }
+      break;
+  }
+  return ganador;
+}
---
+++ public/CerebroDeWalter.js
@@ -0,0 +1,366 @@
+const URL = "http://localhost:3000";
+const socket = io(URL, { autoConnect: false });
+
+// Datos de usuario
+let User = {
+  username: "",
+  id: "",
+  roomConnected: 0,
+  jugada: "",
+  puedeJugar: false,
+  puntaje: 0,
+};
+let Enemigo = {
+  username: "",
+  puntaje: 0,
+};
+// Cargar usuario ya creado
+if (localStorage.User) {
+  let usuario = JSON.parse(localStorage.getItem("User"));
+  User.id = usuario.id;
+  User.username = usuario.username;
+  onUsernameSelection(User.username);
+}
+
+// DOM
+const seccionLogin = document.getElementById("login");
+const seccionLobby = document.getElementById("lobby");
+const seccionJuego = document.getElementById("contenedor");
+const usernameTitle = document.getElementById("usernameTitle");
+const nombresResultado = document.getElementById("nombres-resultado");
+// Socket IO
+
+// Recibir usuarios
+socket.on("users", (users) => {
+  users.forEach((user) => {
+    console.log(user);
+    if (user.username == User.username) {
+      User.id = user.userID;
+      let usuario = {
+        id: user.userID,
+        username: user.username,
+      };
+      let save = JSON.stringify(usuario);
+      localStorage.setItem("User", save);
+    }
+  });
+  seccionLogin.style.display = "none";
+  usernameTitle.innerText = User.username;
+  seccionLobby.style.display = "flex";
+});
+// Recibir error
+socket.on("error", (msg) => {
+  console.log(msg.msg);
+});
+// Recibir datos de salas
+let sala = 1;
+socket.on("datos de salas", (msg) => {
+  console.log(msg);
+  sala = 1;
+  for (let room of msg.rooms) {
+    actualizarSalas(sala, room);
+    sala++;
+  }
+});
+// Conectar a los rooms
+// Room 1
+const roomUnoBoton = document.getElementById("room1");
+roomUnoBoton.addEventListener("click", () => {
+  let msg = {
+    username: User.username,
+    id: User.id,
+  };
+  socket.emit("conectarRoom1", msg);
+});
+// Room 2
+const roomDosBoton = document.getElementById("room2");
+roomDosBoton.addEventListener("click", () => {
+  let msg = {
+    username: User.username,
+    id: User.id,
+  };
+  socket.emit("conectarRoom2", msg);
+});
+// Room 3
+const roomTresBoton = document.getElementById("room3");
+roomTresBoton.addEventListener("click", () => {
+  let msg = {
+    username: User.username,
+    id: User.id,
+  };
+  socket.emit("conectarRoom3", msg);
+});
+// Room 4
+const roomCuatroBoton = document.getElementById("room4");
+roomCuatroBoton.addEventListener("click", () => {
+  let msg = {
+    username: User.username,
+    id: User.id,
+  };
+  socket.emit("conectarRoom4", msg);
+});
+// Recibe que se conecta
+socket.on("connected to room", (msg) => {
+  console.log(msg);
+  User.roomConnected = msg.room;
+  seccionLobby.style.display = "none";
+  seccionJuego.style.display = "flex";
+  for (let user of msg.users) {
+    if (user != User.username) {
+      Enemigo.username = user;
+    }
+  }
+  if (msg.users.length == 2) {
+    User.puedeJugar = true;
+    WalterHabla.innerText = `Podemos empezar a jugar`;
+    nombresResultado.innerText = `${User.username} - ${Enemigo.username}`;
+  }
+});
+// Recibe que alguien se conecto
+// Room users
+socket.on("new connection room", (msg) => {
+  actualizarSalas(msg.room, msg.users);
+});
+
+// Funciones
+// Actualizar stock salas
+function actualizarSalas(room, users) {
+  const roomUsersUno = document.getElementById("room1Users");
+  const roomUsersDos = document.getElementById("room2Users");
+  const roomUsersTres = document.getElementById("room3Users");
+  const roomUsersCuatro = document.getElementById("room4Users");
+  switch (room) {
+    case 1:
+      roomUsersUno.innerText = `${users.length}/2`;
+      break;
+    case 2:
+      roomUsersDos.innerText = `${users.length}/2`;
+      break;
+    case 3:
+      roomUsersTres.innerText = `${users.length}/2`;
+      break;
+    case 4:
+      roomUsersCuatro.innerText = `${users.length}/2`;
+      break;
+  }
+}
+
+// Conectar
+function onUsernameSelection(username) {
+  socket.auth = { username };
+  socket.connect();
+}
+
+// Login
+const botonLogin = document.getElementById("iniciarBoton");
+const inputUsername = document.getElementById("inputUsername");
+
+botonLogin.addEventListener("click", () => {
+  if (inputUsername.value.length < 5) return;
+  onUsernameSelection(inputUsername.value);
+  User.username = inputUsername.value;
+});
+
+// EL JUEGO
+
+// botones
+const BotonPiedra = document.getElementById("piedra");
+const BotonPapel = document.getElementById("papel");
+const BotonTijera = document.getElementById("tijera");
+
+// Elementos de texto
+const Respuesta = document.getElementById("respuesta");
+const WalterHabla = document.getElementById("walterHabla");
+const Resultado = document.getElementById("resultado");
+// Variables
+let puntajeJugador = 0;
+let puntajeWalter = 0;
+let opcionesDeJuego = ["Piedra", "Papel", "Tijera"];
+
+// Eleccion del jugador local
+BotonPiedra.addEventListener("click", () => {
+  if (User.puedeJugar) {
+    emitirAServidor("Piedra");
+    BotonPiedra.style.backgroundColor = "green";
+  }
+});
+
+BotonPapel.addEventListener("click", () => {
+  if (User.puedeJugar) {
+    emitirAServidor("Papel");
+    BotonPapel.style.backgroundColor = "green";
+  }
+});
+
+BotonTijera.addEventListener("click", () => {
+  if (User.puedeJugar) {
+    emitirAServidor("Tijera");
+    BotonTijera.style.backgroundColor = "green";
+  }
+});
+
+// Emitir a Servidor
+function emitirAServidor(jugada) {
+  let Jugada = {
+    jugada: jugada,
+    room: User.roomConnected,
+    username: User.username,
+  };
+  User.jugada = jugada;
+  User.yaJugo = true;
+  socket.emit("jugada", Jugada);
+  WalterHabla.innerText = `Esperando a ${Enemigo.username}...`;
+}
+
+// Listener de resultado
+socket.on("resultado", (msg) => {
+  if (msg == User.username) {
+    User.puntaje++;
+    WalterHabla.innerText = `Ganaste!`;
+    switch (User.jugada) {
+      case "Piedra":
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        break;
+      case "Papel":
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        break;
+      case "Tijera":
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        break;
+    }
+  } else if (msg == "Empate") {
+    WalterHabla.innerText = `Empate!`;
+    switch (User.jugada) {
+      case "Piedra":
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        break;
+      case "Papel":
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        break;
+      case "Tijera":
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        break;
+    }
+  } else {
+    Enemigo.puntaje++;
+    WalterHabla.innerText = `Perdiste!`;
+    switch (User.jugada) {
+      case "Piedra":
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        break;
+      case "Papel":
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        break;
+      case "Tijera":
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        break;
+    }
+  }
+  Resultado.innerText = `${User.puntaje} - ${Enemigo.puntaje}`;
+  setTimeout(() => {
+    BotonPiedra.removeAttribute("style");
+    BotonPapel.removeAttribute("style");
+    BotonTijera.removeAttribute("style");
+  }, 1000 * 2);
+});
+
+// Funciones del juego
+function resolverJuego(eleccionDeJugador) {
+  let eleccionDeWalter =
+    opcionesDeJuego[Math.floor(Math.random() * opcionesDeJuego.length)];
+  resolverResultado(eleccionDeJugador, eleccionDeWalter);
+}
+
+function resolverResultado(jugador, walter) {
+  Respuesta.removeAttribute("style");
+  switch (jugador) {
+    case "Piedra":
+      if (walter == "Papel") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        actualizarPuntaje("pierde", jugador);
+      } else if (walter == "Tijera") {
+        Respuesta.style.transform = "rotate(270deg)";
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        actualizarPuntaje("gana", jugador);
+      } else if (walter == "Piedra") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        actualizarPuntaje("empate", jugador);
+      }
+      break;
+
+    case "Papel":
+      if (walter == "Papel") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        actualizarPuntaje("empate", jugador);
+      } else if (walter == "Tijera") {
+        Respuesta.style.transform = "rotate(270deg)";
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        actualizarPuntaje("pierde", jugador);
+      } else if (walter == "Piedra") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        actualizarPuntaje("gana", jugador);
+      }
+      break;
+
+    case "Tijera":
+      if (walter == "Papel") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-paper"></i>`;
+        actualizarPuntaje("gana", jugador);
+      } else if (walter == "Tijera") {
+        Respuesta.style.transform = "rotate(270deg)";
+        Respuesta.innerHTML = `<i class="fas fa-hand-scissors"></i>`;
+        actualizarPuntaje("empate", jugador);
+      } else if (walter == "Piedra") {
+        Respuesta.innerHTML = `<i class="fas fa-hand-rock"></i>`;
+        actualizarPuntaje("pierde", jugador);
+      }
+      break;
+  }
+}
+
+function actualizarPuntaje(caso, jugadaJugador) {
+  switch (caso) {
+    case "gana":
+      puntajeJugador++;
+      WalterHabla.innerText = `Uh me ganaste`;
+      if (jugadaJugador == "Piedra") {
+        backgroundColor(BotonPiedra, "green");
+      } else if (jugadaJugador == "Tijera") {
+        backgroundColor(BotonTijera, "green");
+      } else if (jugadaJugador == "Papel") {
+        backgroundColor(BotonPapel, "green");
+      }
+      break;
+
+    case "pierde":
+      puntajeWalter++;
+      WalterHabla.innerText = `No sabes sumar, beso`;
+      if (jugadaJugador == "Piedra") {
+        backgroundColor(BotonPiedra, "red");
+      } else if (jugadaJugador == "Tijera") {
+        backgroundColor(BotonTijera, "red");
+      } else if (jugadaJugador == "Papel") {
+        backgroundColor(BotonPapel, "red");
+      }
+      break;
+
+    case "empate":
+      WalterHabla.innerText = `Bue empatamos`;
+      if (jugadaJugador == "Piedra") {
+        backgroundColor(BotonPiedra, "yellow");
+      } else if (jugadaJugador == "Tijera") {
+        backgroundColor(BotonTijera, "yellow");
+      } else if (jugadaJugador == "Papel") {
+        backgroundColor(BotonPapel, "yellow");
+      }
+      break;
+  }
+  Resultado.innerText = `${puntajeJugador} - ${puntajeWalter}`;
+}
+
+function backgroundColor(objeto, color) {
+  objeto.style.backgroundColor = color;
+  setTimeout(() => {
+    objeto.removeAttribute("style");
+  }, 400);
+}
