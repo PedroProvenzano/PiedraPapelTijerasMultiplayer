b7ea12970574d7a0c2bbeaccac05c996796164e0
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-30 03:35:38 -0300

    fix cors?

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ require("dotenv/config");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://walter-multiplayer.herokuapp.com/",
+    methods: ["GET", "POST"],
+  },
+});
 const port = process.env.PORT;
 
 // mongoDB();
