66662c2f075fc01db2bf3f72b8653ccb4963f5d1
Pedro Provenzano <pedroprov0@gmail.com>
2021-03-30 16:00:56 -0300

    fix 6

--- app.js
+++ app.js
@@ -389,7 +389,7 @@ io.on("connection", (socket) => {
     let salas = {
       rooms: [room1, room2, room3, room4],
     };
-    socket.broadcast.emit("datos de salas", salas);
+    io.emit("datos de salas", salas);
     let msge = {
       user: socket.username,
       room: msg.room,
